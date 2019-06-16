faeee8f7a2997a1151104d12796c84b175088e5f
Courtney Fink <courtney55@knights.ucf.edu>
2019-06-16 06:00:11 +0000

    set up geocoding

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode
+  
+  validates :name, presence: true, length: { minimum: 3, too_short: "must be at least %{count} characters long"}
+  validates :address, :description, presence: true
 end
